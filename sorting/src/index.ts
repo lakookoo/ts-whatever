5e200b73fed141c091ec8e897769013252554a5a
Larisa <kosareva.larisa@gmail.com>
2024-06-15 19:49:48 +0200

    add abstract class

--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -4,15 +4,13 @@ import { CharacterCollection } from './CharactersCOllection';
 import { LinkedList } from './LinkedList';
 
 
-// const numbColl = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbColl);
-// sorter.sort();
-// console.log(numbColl.data);
+const numbColl = new NumbersCollection([10, 3, -5, 0]);
+numbColl.sort();
+console.log(numbColl.data);
 
-// const chars = new CharacterCollection('Xanb');
-// const sorter = new Sorter(chars);
-// sorter.sort();
-// console.log(chars);
+const chars = new CharacterCollection('Xanb');
+chars.sort();
+console.log(chars);
 
 const ll = new LinkedList();
 ll.add(500);
@@ -20,7 +18,5 @@ ll.add(-10);
 ll.add(-3);
 ll.add(4);
 
-const sorter = new Sorter(ll);
-
-sorter.sort();
+ll.sort();
 ll.print();
