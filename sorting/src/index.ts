a9acf85b88b16ea8b441faecb30a71a305492209
Larisa <kosareva.larisa@gmail.com>
2024-06-15 19:29:43 +0200

    add linkedList

--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharacterCollection } from './CharactersCOllection';
+import { LinkedList } from './LinkedList';
 
 
 // const numbColl = new NumbersCollection([10, 3, -5, 0]);
@@ -8,7 +9,18 @@ import { CharacterCollection } from './CharactersCOllection';
 // sorter.sort();
 // console.log(numbColl.data);
 
-const chars = new CharacterCollection('Xanb');
-const sorter = new Sorter(chars);
+// const chars = new CharacterCollection('Xanb');
+// const sorter = new Sorter(chars);
+// sorter.sort();
+// console.log(chars);
+
+const ll = new LinkedList();
+ll.add(500);
+ll.add(-10);
+ll.add(-3);
+ll.add(4);
+
+const sorter = new Sorter(ll);
+
 sorter.sort();
-console.log(chars);
+ll.print();
